9ab5373c506f7fa007e123590771ec1508340013
CHOWDARY SARITHA <142637859+Chowdarysaritha311@users.noreply.github.com>
2025-08-02 20:31:57 +0530

    foodData.js

---
+++ foodData.js
@@ -0,0 +1,40 @@
+const foodData = [
+  {
+    name: "Oats & Banana Bowl",
+    category: "Morning",
+    ageRange: [15, 40],
+    workType: ["Moderate", "Active"],
+    calories: 250,
+    benefits: "Boosts energy & improves digestion",
+    image: "https://images.unsplash.com/photo-1584270354949-1ddf2c786e09"
+  },
+  {
+    name: "Quinoa & Veggie Salad",
+    category: "Afternoon",
+    ageRange: [20, 60],
+    workType: ["Sedentary", "Moderate"],
+    calories: 300,
+    benefits: "Rich in fiber and plant protein",
+    image: "https://images.unsplash.com/photo-1606756792060-0f0b2b2334d1"
+  },
+  {
+    name: "Grilled Chicken with Veggies",
+    category: "Night",
+    ageRange: [25, 50],
+    workType: ["Active", "Moderate"],
+    calories: 400,
+    benefits: "High protein & muscle recovery",
+    image: "https://images.unsplash.com/photo-1613145996070-b1ffcea2b20b"
+  },
+  {
+    name: "Fruit Smoothie",
+    category: "Morning",
+    ageRange: [15, 30],
+    workType: ["Sedentary", "Moderate", "Active"],
+    calories: 200,
+    benefits: "Rich in vitamins & hydration",
+    image: "https://images.unsplash.com/photo-1572441710533-6226e1e8c15e"
+  }
+];
+
+export default foodData;
